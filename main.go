3d2bd10aca9064446a0e241a7b35888af0cf8f93
bb0i <hrshbali10@gmail.com>
2022-07-19 09:21:26 +0100

    Initial commit: README + Basics in MAIN.GO

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Go Basics
+	fmt.Println("1. Initialize project by using command:")
+	fmt.Println("	   Go mod init <module path>")
+	fmt.Println("2. GO PROGRAMS ARE ORGANISED INTO PACKAGES:")
+	fmt.Println("		e.g. this code belongs to main package")
+	fmt.Println("3. Declare the entry point to start the project:")
+	fmt.Println("		func main(){<code>} is always the entry point")
+	fmt.Println("4. To run the project use commang: ")
+	fmt.Println("		go run main.go")
+
+}
